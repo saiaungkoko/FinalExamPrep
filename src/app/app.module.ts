982238896068440467519ed93dcfe0bcd4002245
saiaungkoko <saiaungkoko@mail.fresnostate.edu>
2020-05-11 21:32:26 -0700

    End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,13 +24,16 @@ import { BooksComponent } from './books/books.component';
 import { BookCreateComponent } from './books/book-create/book-create.component';
 
 // beverages components
+import { UserService } from './users/user.service';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
+import { UserCreateComponent } from './users/user-create/user-create.component';
+
+// users components
 import { BeverageService } from './beverages/beverage.service';
 import { BeverageComponent } from './beverages/beverage/beverage.component';
 import { BeveragesComponent } from './beverages/beverages.component';
 import { BeverageCreateComponent } from './beverages/beverage-create/beverage-create.component';
-
-// users components
-// ...
 
 // Routes
 const appRoutes: Routes = [
@@ -46,7 +49,9 @@ const appRoutes: Routes = [
   { path: 'beverage/:id', component: BeverageComponent },
   { path: 'beverage-create', component: BeverageCreateComponent },
   // users
-  // ...
+  { path: 'users', component: UsersComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: 'user-create', component: UserCreateComponent },
 ];
 
 @NgModule({
@@ -64,9 +69,11 @@ const appRoutes: Routes = [
     // beverages
     BeveragesComponent,
     BeverageCreateComponent,
-    BeverageComponent
+    BeverageComponent,
     // users
-    // ...
+    UserComponent,
+    UsersComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -78,7 +85,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     BookService,
-    BeverageService
+    BeverageService,
+    UserService
     // ...
   ],
   bootstrap: [AppComponent]
